dc82ad93192caef51c91a668d24ce27f00776d49
SJ <sj1004class@hanmail.net>
2022-03-29 20:54:02 +0900

    first

---
+++ homepage2022/src/main/java/egovframework/let/temp/service/impl/TempDAO.java
@@ -0,0 +1,16 @@
+package egovframework.let.temp.service.impl;
+import egovframework.let.temp.service.TempVO;
+import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository("tempDAO")
+public class TempDAO extends EgovAbstractDAO {
+
+    
+    public TempVO selectTemp(TempVO boardVO) throws Exception {
+    	return (TempVO)select("TempDAO.selectTemp", boardVO);
+    }
+
+}
---
+++ homepage2022/src/main/java/egovframework/let/temp/service/impl/TempServiceImpl.java
@@ -0,0 +1,73 @@
+package egovframework.let.temp.service.impl;
+
+import egovframework.let.temp.service.TempService;
+import egovframework.let.temp.service.TempVO;
+
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+
+@Service("tempService")
+public class TempServiceImpl extends EgovAbstractServiceImpl implements TempService {
+
+    @Resource(name = "propertiesService")
+    protected EgovPropertyService propertyService;
+    /*
+    @Resource(name="tempMapper")
+	private TempMapper tempMapper;
+    */
+    @Resource(name = "tempDAO")
+    private TempDAO tempDAO;
+    /*
+    @Resource(name = "egovTempIdGnrService")
+    private EgovIdGnrService idgenService;
+    */
+    /*
+	@Override
+	public TempVO selectTemp(TempVO vo) throws Exception {
+		return tempMapper.selectTemp(vo);
+	}
+	
+	public String insertTemp(TempVO vo) throws Exception {
+		String id = idgenService.getNextStringId();
+		vo.setTempId(id);
+		tempMapper.insertTemp(vo);
+		
+		return id;
+	}
+	
+	//임시데이터 목록 가져오기
+	public List<EgovMap> selectTempList(TempVO vo) throws Exception {
+		return tempMapper.selectTempList(vo);
+	}
+	
+	//임시데이터 목록 수
+	public int selectTempListCnt(TempVO vo) throws Exception {
+		return tempMapper.selectTempListCnt(vo);
+	}
+	
+	//임시데이터 수정하기
+	public void updateTemp(TempVO vo) throws Exception{
+		tempMapper.updateTemp(vo);
+	}
+	
+	//임시데이터 삭제하기
+	public void deleteTemp(TempVO vo) throws Exception{
+		tempMapper.deleteTemp(vo);
+	}
+	*/
+    
+	@Override
+	public TempVO selectTemp(TempVO vo) throws Exception {
+		return tempDAO.selectTemp(vo);
+	}
+	
+}
