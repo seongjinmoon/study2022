3dba124dc6f25857f04ad158a66b6b1eb28f048b
SJ <sj1004class@hanmail.net>
2022-09-13 21:46:11 +0900

    5주차 수업 자료

---
+++ homepage2022/src/main/java/egovframework/let/rsv/service/impl/ReservationApplyMapper.java
@@ -0,0 +1,50 @@
+package egovframework.let.rsv.service.impl;
+
+import java.util.List;
+
+import egovframework.let.rsv.service.ReservationApplyVO;
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+@Mapper("reservationApplyMapper")
+public interface ReservationApplyMapper {
+	
+	//예약자 등록하기
+	void insertReservationApply(ReservationApplyVO vo) throws Exception;
+	
+	//기존 신청여부
+	int duplicateApplyCheck(ReservationApplyVO vo) throws Exception;
+		
+	//예약자 목록 가져오기
+	List<EgovMap> selectReservationApplyList(ReservationApplyVO vo) throws Exception;
+	
+	//예약자 목록 수
+	int selectReservationApplyListCnt(ReservationApplyVO vo) throws Exception;
+		
+	//예약자 상세정보
+	ReservationApplyVO selectReservationApply(ReservationApplyVO vo) throws Exception;
+	
+	//예약자 수정하기
+	void updateReservationApply(ReservationApplyVO vo) throws Exception;
+	
+	//예약자 삭제하기
+	void deleteReservationApply(ReservationApplyVO vo) throws Exception;
+	
+	//예약자 승인처리
+	void updateReservationConfirm(ReservationApplyVO vo) throws Exception;
+	
+	//예약가능여부 확인
+	void rsvCheck(ReservationApplyVO vo) throws Exception;
+	
+	//임시예약자 등록하기
+	void insertReservationApplyTemp(ReservationApplyVO vo) throws Exception;
+	
+	//임시예약자 삭제
+	void deleteReservationApplyTemp(ReservationApplyVO vo) throws Exception;
+	
+	//임시예약자 목록 가져오기
+	List<EgovMap> selectReservationApplyTemp(ReservationApplyVO vo) throws Exception;
+	
+	//임시예약자 예약자로 일괄등록
+	void insertReservationApplyTempAll(ReservationApplyVO vo) throws Exception;
+}
